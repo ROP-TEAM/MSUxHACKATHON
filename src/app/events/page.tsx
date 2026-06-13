ea40e042740a885a83a232a0bf10cd19d2da0b7f
Pattararnxx <pattaraporn.po@kkumail.com>
2026-06-14 05:24:00 +0700

    improve ui

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,13 +2,22 @@ import { Footer } from "@/components/footer/footer";
 import { ALL_POSTERS, CATEGORY_SECTIONS } from "@/components/home/homeData";
 import EventsPageClient from "./EventsPageClient";
 import styles from "./page.module.scss";
+import Image from "next/image";
 
 const CATEGORIES = CATEGORY_SECTIONS.map((s) => s.title);
 
 const EventsPage = () => {
   return (
     <div className={styles.page}>
-      <div className={styles.hero} />
+      <Image
+              src="/image/background1.svg"
+              alt="background"
+              width={400}
+              height={250}
+              style={{
+                width: "100%",
+                height: "100%",
+              }}/>
 
       <div className={styles.eventsArea}>
         <EventsPageClient allEvents={ALL_POSTERS} categories={CATEGORIES} />
