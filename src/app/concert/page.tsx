204604ca10475ccd80a632f92ad6d499c8b3f22d
Pattararnxx <pattaraporn.po@kkumail.com>
2026-06-13 23:34:54 +0700

    changename

--- src/app/concert/page.tsx
+++ src/app/concert/page.tsx
@@ -2,7 +2,7 @@ import StageOverview from "@/components/stageOverview/StageOverview";
 import styles from "./page.module.scss";
 const ConcertPage = () => {
     return (
-        <div className={styles.concertPage}>
+        <div className={styles.container}>
             <div className={styles.stage}>
                 <StageOverview></StageOverview>
             </div>
